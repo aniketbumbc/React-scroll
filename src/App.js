4d3f03625cf53800f77554359cfdb03f97be25da
aniket <aniket.umbc@gmail.com>
2021-06-05 12:25:11 +0530

    infinite scrolling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useRef, useCallback } from 'react';
 import './App.css';
+import useBookSearch from './Hooks/useBookSearch';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+  const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
+  const obse = useRef();
+  const lastBookEle = useCallback(
+    (lastEle) => {
+      if (loading) return;
+      if (obse.current) {
+        obse.current.disconnect();
+      }
+      obse.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevNumber) => prevNumber + 1);
+        }
+      });
+      if (lastEle) {
+        obse.current.observe(lastEle);
+      }
+    },
+    [loading, hasMore]
+  );
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return (
+            <div ref={lastBookEle} key={book}>
+              {book}
+            </div>
+          );
+        } else {
+          return <div key={book}>{book}</div>;
+        }
+      })}
+
+      <div>{loading && 'Loading......'}</div>
+      <div>{error && 'Error'}</div>
     </div>
   );
 }
